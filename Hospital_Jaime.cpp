c7ea8069bdee1e32a7e0d0db9f457a70f31f4cb4
Jaimefcs <jfcanetes@gmail.com>
2025-01-15 19:27:36 +0100

    Agregar archivos de proyecto.

---
+++ Hospital_Jaime.cpp
@@ -0,0 +1,12 @@
+﻿// Hospital_Jaime.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Hospital_Jaime.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
